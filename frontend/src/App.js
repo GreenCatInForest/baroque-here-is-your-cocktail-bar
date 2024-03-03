f250a151d97295e0ccd25bb831f4a28d37753547
GreenCatInForest <event2016.2017@gmail.com>
2024-03-03 18:34:49 +0000

    add usestate dark mode

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,11 @@
+import { useState } from "react";
+
 import { NavBar } from "./components/NavBar";
 import { Banner } from "./components/Banner";
 
 export const App = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
   return (
     <div className="App">
       <NavBar />
