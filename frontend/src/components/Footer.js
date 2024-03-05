e60634b8ecd1ef84540d1055da4f6a9ca5addd05
GreenCatInForest <event2016.2017@gmail.com>
2024-03-05 13:07:43 +0000

    add footer content

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,2 +1,6 @@
 export const Footer = () => {
-    return <div>Footer</div>}
+  const day = new Date(2024, 3, 5).toDateString();
+  return (
+    <div className="footerWrapper">&#9400; YK | YKApps | deployed on {day}</div>
+  );
+};
