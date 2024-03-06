3928b758478f6cabf4446a8ab7cf1bc2ab93a1f4
GreenCatInForest <event2016.2017@gmail.com>
2024-03-06 00:48:22 +0000

    add render items in local storage

--- frontend/src/pages/YourCocktailSelection.js
+++ frontend/src/pages/YourCocktailSelection.js
@@ -1,7 +1,28 @@
+import { CocktailCard } from "../components/Cocktails/CocktailCard";
+
 export const YourCocktailSelection = () => {
+  const existingFavorites = JSON.parse(localStorage.getItem("favorites")) || [];
+
   return (
     <div>
-      <h1>Your Cocktail Selection</h1>
+      {!existingFavorites ? (
+        <h2>You have no cocktails yet, let's find something for you!</h2>
+      ) : (
+        <h1>Your Cocktail Selection</h1>
+      )}
+
+      {existingFavorites.length > 0 ? (
+        <div className="cocktailsMiniContainer">
+          {existingFavorites.map((existingFavorite) => (
+            <CocktailCard
+              key={existingFavorite.key}
+              item={existingFavorite.data}
+            />
+          ))}
+        </div>
+      ) : (
+        ""
+      )}
     </div>
   );
 };
