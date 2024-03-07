2a0c7f2376fdc88047754535d9d958dcebf76c86
GreenCatInForest <event2016.2017@gmail.com>
2024-03-07 12:10:03 +0000

    youtube API fetch is working, env corrected

--- frontend/src/components/YouTubeForm.js
+++ frontend/src/components/YouTubeForm.js
@@ -6,11 +6,13 @@ export const YouTubeForm = () => {
   const [queryYouTube, setQueryYouTube] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
-  const apiKey = process.env.YOUTUBE_API_KEY;
+  const apiKey = process.env.REACT_APP_XRapidAPIYOUTUBE_API_KEY;
+  console.log(apiKey);
 
   const handleSubmit = (event) => {
     event.preventDefault();
     console.log("submitted");
+    fetchCocktailsYouTube(queryYouTube);
   };
 
   const handleChange = (event) => {
@@ -18,6 +20,27 @@ export const YouTubeForm = () => {
   };
 
   console.log(queryYouTube);
+
+  const fetchCocktailsYouTube = async (queryYouTube) => {
+    try {
+      setIsLoading(true);
+      const response = await axios.get(
+        `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25`,
+        {
+          params: {
+            q: `${queryYouTube} cocktail recipe`,
+            key: apiKey,
+          },
+        }
+      );
+      setDataYouTube(response.data.items);
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+      setError(error.message);
+    }
+  };
+  console.log(dataYouTube);
 
   return (
     <div>
