c7edacec1cbcd240c584cce31d8ed6b85e9c880f
GreenCatInForest <event2016.2017@gmail.com>
2024-03-08 11:15:00 +0000

    add img to home page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,6 +2,16 @@ import { useFormContext } from "../contexts/formContext";
 import { Banner } from "../components/Banner";
 import { Form } from "../components/Form";
 import { CocktailsContainer } from "../components/Cocktails/CocktailsContainer";
+import homeFormSectionImgLeft from "../assets/images/glass-29509_640.png";
+import homeFormSectionImgRight from "../assets/images/image_fx_fhoto_of_a_smiling_statue_woman.jpg";
+// "image_fx_fhoto_of_a_happy_greek_statue_enjoying_the_wi.jpg"
+// "drink-2029520_1280.png"
+// "bacchus-7330206_1280.png"
+// "glass.png"
+// "glass-29509_640.png"
+// "champagne-29051_1280.png"
+// "cup-5215986_1280.png"
+// "image_fx_fhoto_of_a_smiling_statue_woman.jpg"
 
 export const Home = () => {
   // state for search queries
@@ -11,13 +21,25 @@ export const Home = () => {
   return (
     <div className="theme">
       <Banner />
-      <Form
-        query={query}
-        setQuery={setQuery}
-        setQuerySubmitted={setQuerySubmitted}
-        querySubmitted={querySubmitted}
-        templateExplore="" // empty string to remove formStyleExplore class
-      />
+      <div className="homeFormSection">
+        <img
+          src={homeFormSectionImgLeft}
+          alt="cocktail"
+          className="homeImage w-36"
+        />
+        <Form
+          query={query}
+          setQuery={setQuery}
+          setQuerySubmitted={setQuerySubmitted}
+          querySubmitted={querySubmitted}
+          templateExplore="" // empty string to remove formStyleExplore class
+        />
+        <img
+          src={homeFormSectionImgRight}
+          alt="cocktail"
+          className="homeImage w-64"
+        />
+      </div>
       <CocktailsContainer
         query={query}
         setQuery={setQuery}
