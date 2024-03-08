736fdae6f9f8fcfb87ca21b0e7f0ef15f2d5a20a
GreenCatInForest <event2016.2017@gmail.com>
2024-03-08 13:11:47 +0000

    style home

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,19 +1,28 @@
+import { useThemeContext } from "../contexts/themeContext";
 import { useFormContext } from "../contexts/formContext";
 import { Banner } from "../components/Banner";
 import { Form } from "../components/Form";
 import { CocktailsContainer } from "../components/Cocktails/CocktailsContainer";
-import homeFormSectionImgLeft from "../assets/images/glass-29509_640.png";
-import homeFormSectionImgRight from "../assets/images/image_fx_fhoto_of_a_smiling_statue_woman.jpg";
+import homeFormSectionImgLeft from "../assets/images/bacchus-7330206_1280.png";
+import homeFormSectionImgLeftDark from "../assets/images/champagne-29051_1280.png";
+import homeFormSectionImgRightDark from "../assets/images/image_fx_fhoto_of_a_smiling_statue_woman.jpg";
+import homeFormSectionImgRight from "../assets/images/cup-5215986_1280.png";
 // "image_fx_fhoto_of_a_happy_greek_statue_enjoying_the_wi.jpg"
 // "drink-2029520_1280.png"
 // "bacchus-7330206_1280.png"
 // "glass.png"
 // "glass-29509_640.png"
 // "champagne-29051_1280.png"
+// "champagne-29051_1280.png"
+// "the-statue-3099675_1280.png"
 // "cup-5215986_1280.png"
 // "image_fx_fhoto_of_a_smiling_statue_woman.jpg"
 
 export const Home = () => {
+  // state for theme context
+
+  const { isDarkTheme } = useThemeContext();
+
   // state for search queries
   const { querySubmitted, setQuerySubmitted, query, setQuery, data, setData } =
     useFormContext();
@@ -23,9 +32,11 @@ export const Home = () => {
       <Banner />
       <div className="homeFormSection">
         <img
-          src={homeFormSectionImgLeft}
+          src={
+            isDarkTheme ? homeFormSectionImgLeftDark : homeFormSectionImgLeft
+          }
           alt="cocktail"
-          className="homeImage w-36"
+          className="homeImageLeft"
         />
         <Form
           query={query}
@@ -35,9 +46,11 @@ export const Home = () => {
           templateExplore="" // empty string to remove formStyleExplore class
         />
         <img
-          src={homeFormSectionImgRight}
+          src={
+            !isDarkTheme ? homeFormSectionImgRight : homeFormSectionImgRightDark
+          }
           alt="cocktail"
-          className="homeImage w-64"
+          className={`homeImageRight ${!isDarkTheme}`}
         />
       </div>
       <CocktailsContainer
